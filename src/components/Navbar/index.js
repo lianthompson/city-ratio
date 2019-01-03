c8daf9796bce81451c681bc843e4d0cd40cd79e2
Lian <thompsonlian@yahoo.com>
2019-01-03 15:17:42 -0800

    add router

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import { Search } from '../Search'
 import './Navbar.css';
 
 export class Navbar extends Component {
 
     render() {
         return (
-            <div className="navbar-container">
-                <div className="navbar-logo">city-ratio</div>
-                <div className="navbar-link-container">
-                    {/* <ul className="navbar-item">saved maps</ul> */}
+            <Router>
+                <div className="navbar-container">
+                    <Link to="/">
+                        <div className="navbar-logo">city-ratio</div>
+                    </Link>
+                    <Link to="savedmaps">
+                        <div className="navbar-link-container">
+                            <ul className="navbar-item">saved maps</ul>
+                        </div>
+                    </Link>
+                    {/* <Route path="/" exact component={Search} /> */}
                 </div>
-            </div>
+            </Router>
         )
     }
 }
