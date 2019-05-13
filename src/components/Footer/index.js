f8072b28b340d32f2739da5b482c01753855461c
Lian Thompson <lianythompson@gmail.com>
2019-05-13 10:32:53 -0700

    add route to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './Footer.css';
 
 export class Footer extends Component {
 
     render() {
         return (
+            <Router>
             <div className="footer-container">
                 <a href="mailto:lian@colark.com" className="footer-item-contact">feedback</a>
+                <Link to='howitworks'>
                 <ul className="footer-item">how it works</ul>
+                </Link>
             </div>
+            </Router>
         )
     }
 }
