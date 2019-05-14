a3b26e796654dd44f73828a3e447a6a3ae48eb28
Lian Thompson <lianythompson@gmail.com>
2019-05-14 11:27:39 -0700

    add alt tag

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -40,7 +40,7 @@ export class Search extends Component {
                 <form onSubmit={this.onTextChange} className="search-form-container">
                     <input type='text' id="firstCity" name='firstCity' placeholder='Ex. San Francisco' required 
                     />
-                    <img src="https://res.cloudinary.com/dec0zvcps/image/upload/v1557788848/refershArrow_ylxhaq.png" className="reverse-button" onClick={this.reverse}/>
+                    <img src="https://res.cloudinary.com/dec0zvcps/image/upload/v1557788848/refershArrow_ylxhaq.png" className="reverse-button" onClick={this.reverse} alt=""/>
                     <input type='text' id="secondCity" name='secondCity' placeholder='Ex. New York' required/>
                     <button type='submit' className="search-button">compare</button>
                 </form>
