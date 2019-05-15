331e37246297fbe030d70110a761b37299081177
Lian Thompson <lianythompson@gmail.com>
2019-05-15 14:45:47 -0700

    change opacity

--- src/components/ActiveMap/index.js
+++ src/components/ActiveMap/index.js
@@ -1,6 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import mapboxgl from 'mapbox-gl';
+import turf from 'turf';
+import turf2 from '@turf/point-to-line-distance';
 import './ActiveMap.css';
+
+console.log(turf);
 
 mapboxgl.accessToken = "pk.eyJ1IjoibGlhbnRob21wc29uIiwiYSI6ImNqcGJqMmx3aTA0Z2MzamxrZjhzcmY5c2wifQ.TVNesv3GrmPx2Y87mRiXcg";
 
@@ -8,127 +12,37 @@ mapboxgl.accessToken = "pk.eyJ1IjoibGlhbnRob21wc29uIiwiYSI6ImNqcGJqMmx3aTA0Z2Mza
  var secondCityCoordinates = [-74.006, 40.7128];
  var ZOOM_LEVEL = 12;
 
+ var linestring = {
+    "type": "Feature",
+    "geometry": {
+    "type": "LineString",
+    "coordinates": []
+    }
+    };
 
-// var firstMap = new mapboxgl.Map({
-//     container: this.firstCity, // container id
-//     style: "mapbox://styles/mapbox/basic-v9", //stylesheet location
-//     center: firstCityCoordinates, // starting position [lng, lat]
-//     zoom: ZOOM_LEVEL, // starting zoom
-//     interactive: true,
-//     scrollZoom: false,
-// });
+var geojson = {
+    "type": "FeatureCollection",
+    "features": []
+};
 
-// var secondMap = new mapboxgl.Map({
-//     container: this.secondCity, // container id
-//     style: "mapbox://styles/mapbox/streets-v11", //stylesheet location
-//     center: secondCityCoordinates, // starting position [lng, lat]
-//     zoom: ZOOM_LEVEL, // starting zoom
-//     interactive: true,
-//     scrollZoom: false
-// });
+var from = turf.point([-75.343, 39.984]);
+var to = turf.point([-75.534, 39.123]);
+var options = {units: 'miles'};
 
-// secondMap.on('load', function() {
-//     this.mapTwo.addSource('geojson', {
-//         "type": "geojson",
-//         "data": geojson
-//     });
+// console.log(from)
+ 
+// var distance = turf.distance(from, to, options);
 
-// secondMap.addLayer({
-//     id: 'measure-points',
-//     type: 'circle',
-//     source: 'geojson',
-//     paint: {
-//     'circle-radius': 5,
-//     'circle-color': '#000'
-//     },
-//     filter: ['in', '$type', 'Point']
-//     });
-//     this.mapTwo.addLayer({
-//     id: 'measure-lines',
-//     type: 'line',
-//     source: 'geojson',
-//     layout: {
-//     'line-cap': 'round',
-//     'line-join': 'round'
-//     },
-//     paint: {
-//     'line-color': '#000',
-//     'line-width': 2.5
-//     },
-//     filter: ['in', '$type', 'LineString']
-//     });
+// console.log(distance);
 
-    // this.secondMap.on('click', function(e) {
-        // var features = this.secondMap.queryRenderedFeatures(e.point, { layers: ['measure-points'] });
-         
-        // Remove the linestring from the group
-        // So we can redraw it based on the points collection
-        // if (geojson.features.length > 1) geojson.features.pop();
-         
-        // Clear the Distance container to populate it with a new value
-        // distanceContainer.innerHTML = '';
-         
-        // If a feature was clicked, remove it from the map
-        // if (features.length) {
-        // var id = features[0].properties.id;
-        // geojson.features = geojson.features.filter(function(point) {
-        // return point.properties.id !== id;
-        // });
-        // } else {
-        // var point = {
-        // "type": "Feature",
-        // "geometry": {
-        // "type": "Point",
-        // "coordinates": [
-        // e.lngLat.lng,
-        // e.lngLat.lat
-        // ]
-        // },
-        // "properties": {
-        // "id": String(new Date().getTime())
-        // }
-        // };
-         
-        // geojson.features.push(point);
-        // }
-         
-        // if (geojson.features.length > 1) {
-        // linestring.geometry.coordinates = geojson.features.map(function(point) {
-        // return point.geometry.coordinates;
-        // });
-         
-        // geojson.features.push(linestring);
-         
-        // Populate the distanceContainer with total distance
-        // var value = document.createElement('pre');
-        // value.textContent = 'Total distance: ' + turf.lineDistance(linestring).toLocaleString() + 'km';
-        // distanceContainer.appendChild(value);
-        // }
-         
-        // this.secondMap.getSource('geojson').setData(geojson);
-        // });
-        // });
-         
-        // this.secondMap.on('mousemove', function (e) {
-        // var features = this.secondMap.queryRenderedFeatures(e.point, { layers: ['measure-points'] });
-        // UI indicator for clicking/hovering a point on the map
-        // this.secondMap.getCanvas().style.cursor = (features.length) ? 'pointer' : 'crosshair';
-        // });
 var geocoder = window.L.mapbox.geocoder("mapbox.places", { accessToken: "pk.eyJ1IjoibGlhbnRob21wc29uIiwiYSI6ImNqcGJqMmx3aTA0Z2MzamxrZjhzcmY5c2wifQ.TVNesv3GrmPx2Y87mRiXcg" })
 
-// var geojson = {
-//     "type": "FeatureCollection",
-//     "features": []
-// };
-
-// var linestring = {
-//     "type": "Feature",
-//     "geometry": {
-//         "type": "LineString",
-//         "coordinates": []
-//     }
-// };
 // look up setZoom button in mapboxgl api
+
+function addPoint (e) {
+    e.preventDefault();
+
+}
 
 export class ActiveMap extends Component {
 
@@ -167,7 +81,7 @@ export class ActiveMap extends Component {
             top: 210,
             bottom: 50,
             width: '80%',
-            opacity: '0.6',
+            opacity: '.8',
           };
 
         const secondCityStyle = {
@@ -182,7 +96,7 @@ export class ActiveMap extends Component {
         console.log(this.props.secondCity);
         
         return (
-            <div className="map-container">
+            <div className="map-container" onClick={addPoint}>
             <div id='distance' className='distance-container'></div>
             <div style= {firstCityStyle} ref= {el => this.firstCity = el}/>
             <div style= {secondCityStyle} ref= {el => this.secondCity = el}/>
