b9fc350e635d4dfa387c6dec5f8a6f496377fbe5
Lian Thompson <lianythompson@gmail.com>
2019-05-14 19:23:52 -0700

    remove footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './Footer.css';
 
 export class Footer extends Component {
@@ -9,9 +9,6 @@ export class Footer extends Component {
             <Router>
             <div className="footer-container">
                 <a href="mailto:lian@colark.com" className="footer-item-contact">feedback</a>
-                <Link to='howitworks'>
-                <ul className="footer-item">how it works</ul>
-                </Link>
             </div>
             </Router>
         )
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Navbar.css';
 
+
+// const Child = ({match}) => console.log('match', match) || (
+//     <div>
+//         <h3>id: {match.params.id}</h3>
+//     </div>
+
+// )
+
+
 export class Navbar extends Component {
 
     render() {
@@ -10,14 +19,21 @@ export class Navbar extends Component {
             <Router>
                 <div className="navbar-container">
                     <a className="navbar-logo" href="/">
-                        <img className="navbar-icon" src="https://res.cloudinary.com/dec0zvcps/image/upload/v1557788514/globe_eezsle.jpg" alt=""/>
+                        <img className="navbar-icon" src="https://res.cloudinary.com/dec0zvcps/image/upload/v1557788514/globe_eezsle.jpg" alt="" />
                     </a>
-                    <Link to="savedmaps">
-                        <div className="navbar-link-container">
+                    {/* <Link to="savedmaps"> */}
+                        {/* <div className="navbar-link-container">
                             <ul className="navbar-item"
-                            >saved maps</ul>
+                            >login</ul>
+                        </div> */}
+                        <div className="navbar-link-container">
+                        <ul className="navbar-item">
+                            <a href="mailto:lian@colark.com" className="navbar-item">feedback</a>
+                        </ul>
                         </div>
-                    </Link>
+                    {/* </Link> */}
+
+                    {/* <Route path='/:id' component={Child}/> */}
                 </div>
             </Router>
         )
